234f5229ce18338b057747e9599dc4834f58a02e
Deirdre Weldon <deirdreweldon18@gmail.com>
2019-01-09 14:38:23 +0000

    implement basic views and routes

--- run.py
+++ run.py
@@ -10,8 +10,26 @@ app = Flask(__name__)
 @app.route("/")
 #this is the function which will be bound to our decorator
 def index():
-    return "<h1>Hello There</h1>"
+    
+    """Main page with instructions"""
+    #removing <Hello There>. 
+    #don't use <> for sending message, as this will be interpreted by html and won't display properly.
+    return "To send a message use: /USERNAME/MESSAGE"
+
+#creating routes/views using @app decorator, using <>, this then gets treated as a variable.
+@app.route("/<username>")
+#argument of username
+def user(username):
+    return "Hi " + username
+    
+#creating another @app route decorator for sending message.
+@app.route("/<username>/<message>")
+#creating function which binds to decorator, taking 2 arguments (username and message)
+def send_message(username, message):
+    #using .format method to display message.
+    return "{0}: {1}".format(username, message)
+
 #environmental variables in cloud9 and also which we set ourselves in Heroku.    
 app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
 
-   
+
